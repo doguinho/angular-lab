cba1cf4654f4650be2efa1639238c79983ca874a
Douglas S. Maria <douglas.maria@sinqia.com.br>
2020-07-03 19:27:06 -0300

    usando RxJs para encadeameno de chamadas assíncronas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { of, Observable, interval } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'angular-lab';
+
+  ngOnInit(): void {
+
+    //o mergeMapa é um jeito de fazer várias chamadas de vários tipos, como promisse, observables, 
+    // e comuns encadeadas de forma que a próxima só inicia quando a anterior teve retorno
+
+    of({}).pipe(
+      mergeMap(sucesso => this.teste()),
+      mergeMap(sucesso => interval(5000).pipe(x => this.teste2(sucesso))),
+      mergeMap(sucesso => this.teste3())
+    ).subscribe(
+      
+    )
+
+    console.log('fim');
+    
+  }
+
+  teste():Observable<string>{
+    return new Observable(
+      obs => {
+        console.log('teste1')
+        obs.next('teste1')
+      }
+    )
+  }
+
+  teste2(param:string):Observable<string>{
+    return new Observable(
+      obs => {
+        console.log('teste2')
+        obs.next(param)
+      }
+    )
+  }
+
+  teste3():String{
+    console.log('teste3');
+    
+    return 'teste3'
+  }
+
 }
